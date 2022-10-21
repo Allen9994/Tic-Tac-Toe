b9f279f7047e8c8b48607b8efee595eac148dc21
Allen John <88543723+Allen9994@users.noreply.github.com>
2022-10-21 13:28:59 +0530

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -14,15 +14,20 @@ int main()
 {
     srand (time(NULL));
     int i,j,x=1,k[9] = {0},y[20],z[20],a[3][3],n=1,b=0;
+    int add = 0;
     vector <int> vec={0};
     vector <int> vic={1,2,3,4,5,6,7,8,9};
+    vector <int> pc = {0};
+    vector <int> pl = {0};
+    vector <int> diag = {1,3,7,9};
+    int diagsize = 4;
     
     int p,r = 0,choice;
     string name1= "A",name2 = "B";
     cout<<"WELCOME TO TIC-TAC-TOE";
-    cout<<"\nDo you want to play against player or against PC?\nPress 1 for v/s player\nPress 2 for v/s PC";
+    cout<<"\nDo you want to play against player or against PC?\nPress 1 for v/s player\nPress 2 for v/s PC(DUMB)\nPress 2 for v/s PC(SMART)";
     cin>>choice;
-    choice = choice % 2;
+    choice = choice % 5;
     vector<int>::iterator new_end;
     if(name1 == ""){name1 = "A";}if(name2 == ""){name2 = "B";}
 cout<<"[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]";
@@ -34,10 +39,16 @@ cout<<"[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]";
             {
                 do{
                     cout<<endl<<name1<<" Please enter your choice:";
-                    cin>>p;        system("clear");
+                    cin>>p;        //system("clear");
                 }
                 while(count(vec.begin(), vec.end(), p) != 0);
                 
+                if(p == 1 || p == 3 || p == 7 || p == 9)
+                {
+                    new_end = remove(diag.begin(), diag.end(), p);
+                    diagsize--;
+                }
+                pl.push_back(p);
                 vec.push_back(p);k[n]=p;
                 
                 new_end = remove(vic.begin(), vic.end(), p);
@@ -48,22 +59,132 @@ cout<<"[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]";
                 {
                     do{
                         cout<<endl<<name2<<" Please enter your choice:";
-                        cin>>p;        system("clear");
+                        cin>>p;        //system("clear");
     
                     }
                     while(count(vec.begin(), vec.end(), p) != 0);
                     vec.push_back(p);k[n]=p;
                     }
                 else
+                if(choice == 2)
                 {
                     int siz = vic.size() - vec.size() + 1;
                     int y = rand() % siz;
                     p = vic[y];
                     vec.push_back(p);k[n]=p;
                     new_end = remove(vic.begin(), vic.end(), p);
-                    system("clear");
+                    //system("clear");
                 }
-            }
+                else if(choice == 3)
+                {
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),2) == 0){k[n]=2;}else
+                    if(count(pc.begin(),pc.end(),3) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),7) == 1 && count(pc.begin(),pc.end(),9) == 1 && count(pl.begin(),pl.end(),8) == 0){k[n]=8;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),4) == 0){k[n]=4;}else
+                    if(count(pc.begin(),pc.end(),9) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),6) == 0){k[n]=6;}else
+                    if(count(pc.begin(),pc.end(),9) == 1 && count(pc.begin(),pc.end(),1) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),4) == 1 && count(pl.begin(),pl.end(),7) == 0){k[n]=7;}else
+                    if(count(pc.begin(),pc.end(),7) == 1 && count(pc.begin(),pc.end(),8) == 1 && count(pl.begin(),pl.end(),9) == 0){k[n]=9;}else
+                    if(count(pc.begin(),pc.end(),9) == 1 && count(pc.begin(),pc.end(),6) == 1 && count(pl.begin(),pl.end(),3) == 0){k[n]=3;}else
+                    if(count(pc.begin(),pc.end(),2) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),1) == 0){k[n]=1;}else
+                    if(count(pc.begin(),pc.end(),4) == 1 && count(pc.begin(),pc.end(),6) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),4) == 1 && count(pl.begin(),pl.end(),6) == 0){k[n]=6;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),9) == 0){k[n]=9;}else
+                    if(count(pc.begin(),pc.end(),2) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),8) == 0){k[n]=8;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),2) == 1 && count(pl.begin(),pl.end(),3) == 0){k[n]=3;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),3) == 0){k[n]=3;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),9) == 0){k[n]=9;}else
+                    if(count(pc.begin(),pc.end(),4) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),1) == 0){k[n]=1;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),9) == 1 && count(pl.begin(),pl.end(),1) == 0){k[n]=1;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),7) == 0){k[n]=7;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),9) == 1 && count(pl.begin(),pl.end(),7) == 0){k[n]=7;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),2) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),6) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),4) == 0){k[n]=4;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),2) == 0){k[n]=2;}else
+                    {
+                        int y = rand() % diagsize;
+                        p = diag[y];
+                        diagsize--;
+                        cout<<"  Y"<<y<<"  P"<<y;
+                        pc.push_back(p);
+                        for(i = 0;i<pc.size();i++)
+                        {
+                            cout<<pc[i];
+                        }
+                        vec.push_back(p);k[n]=p;
+                        new_end = remove(vic.begin(), vic.end(), p);
+                        new_end = remove(diag.begin(), diag.end(), p);
+                    }
+                }
+                else if(choice == 4)
+                {cout<<"HI";
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),2) == 0){k[n]=2;}else
+                    if(count(pc.begin(),pc.end(),3) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),7) == 1 && count(pc.begin(),pc.end(),9) == 1 && count(pl.begin(),pl.end(),8) == 0){k[n]=8;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),4) == 0){k[n]=4;}else
+                    if(count(pc.begin(),pc.end(),9) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),6) == 0){k[n]=6;}else
+                    if(count(pc.begin(),pc.end(),9) == 1 && count(pc.begin(),pc.end(),1) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),4) == 1 && count(pl.begin(),pl.end(),7) == 0){k[n]=7;}else
+                    if(count(pc.begin(),pc.end(),7) == 1 && count(pc.begin(),pc.end(),8) == 1 && count(pl.begin(),pl.end(),9) == 0){k[n]=9;}else
+                    if(count(pc.begin(),pc.end(),9) == 1 && count(pc.begin(),pc.end(),6) == 1 && count(pl.begin(),pl.end(),3) == 0){k[n]=3;}else
+                    if(count(pc.begin(),pc.end(),2) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),1) == 0){k[n]=1;}else
+                    if(count(pc.begin(),pc.end(),4) == 1 && count(pc.begin(),pc.end(),6) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),4) == 1 && count(pl.begin(),pl.end(),6) == 0){k[n]=6;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),9) == 0){k[n]=9;}else
+                    if(count(pc.begin(),pc.end(),2) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),8) == 0){k[n]=8;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),2) == 1 && count(pl.begin(),pl.end(),3) == 0){k[n]=3;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),3) == 0){k[n]=3;}else
+                    if(count(pc.begin(),pc.end(),1) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),9) == 0){k[n]=9;}else
+                    if(count(pc.begin(),pc.end(),4) == 1 && count(pc.begin(),pc.end(),7) == 1 && count(pl.begin(),pl.end(),1) == 0){k[n]=1;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),9) == 1 && count(pl.begin(),pl.end(),1) == 0){k[n]=1;}else
+                    if(count(pc.begin(),pc.end(),5) == 1 && count(pc.begin(),pc.end(),3) == 1 && count(pl.begin(),pl.end(),7) == 0){k[n]=7;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),9) == 1 && count(pl.begin(),pl.end(),7) == 0){k[n]=7;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),2) == 1 && count(pl.begin(),pl.end(),5) == 0){k[n]=5;}else
+                    if(count(pc.begin(),pc.end(),6) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),4) == 0){k[n]=4;}else
+                    if(count(pc.begin(),pc.end(),8) == 1 && count(pc.begin(),pc.end(),5) == 1 && count(pl.begin(),pl.end(),2) == 0){k[n]=2;}else
+                    if(count(pl.begin(),pl.end(),1) == 1 && count(pl.begin(),pl.end(),3) == 1 && count(pc.begin(),pc.end(),2) == 0){k[n]=2;}else//
+                    if(count(pl.begin(),pl.end(),3) == 1 && count(pl.begin(),pl.end(),7) == 1 && count(pc.begin(),pc.end(),5) == 0){k[n]=5;}else
+                    if(count(pl.begin(),pl.end(),7) == 1 && count(pl.begin(),pl.end(),9) == 1 && count(pc.begin(),pc.end(),8) == 0){k[n]=8;}else
+                    if(count(pl.begin(),pl.end(),1) == 1 && count(pl.begin(),pl.end(),7) == 1 && count(pc.begin(),pc.end(),4) == 0){k[n]=4;}else
+                    if(count(pl.begin(),pl.end(),9) == 1 && count(pl.begin(),pl.end(),3) == 1 && count(pc.begin(),pc.end(),6) == 0){k[n]=6;}else
+                    if(count(pl.begin(),pl.end(),9) == 1 && count(pl.begin(),pl.end(),1) == 1 && count(pc.begin(),pc.end(),5) == 0){k[n]=5;}else
+                    if(count(pl.begin(),pl.end(),1) == 1 && count(pl.begin(),pl.end(),4) == 1 && count(pc.begin(),pc.end(),7) == 0){k[n]=7;}else
+                    if(count(pl.begin(),pl.end(),7) == 1 && count(pl.begin(),pl.end(),8) == 1 && count(pc.begin(),pc.end(),9) == 0){k[n]=9;}else
+                    if(count(pl.begin(),pl.end(),9) == 1 && count(pl.begin(),pl.end(),6) == 1 && count(pc.begin(),pc.end(),3) == 0){k[n]=3;}else
+                    if(count(pl.begin(),pl.end(),2) == 1 && count(pl.begin(),pl.end(),3) == 1 && count(pc.begin(),pc.end(),1) == 0){k[n]=1;}else
+                    if(count(pl.begin(),pl.end(),4) == 1 && count(pl.begin(),pl.end(),6) == 1 && count(pc.begin(),pc.end(),5) == 0){k[n]=5;}else
+                    if(count(pl.begin(),pl.end(),5) == 1 && count(pl.begin(),pl.end(),4) == 1 && count(pc.begin(),pc.end(),6) == 0){k[n]=6;}else
+                    if(count(pl.begin(),pl.end(),8) == 1 && count(pl.begin(),pl.end(),7) == 1 && count(pc.begin(),pc.end(),9) == 0){k[n]=9;}else
+                    if(count(pl.begin(),pl.end(),2) == 1 && count(pl.begin(),pl.end(),5) == 1 && count(pc.begin(),pc.end(),8) == 0){k[n]=8;}else
+                    if(count(pl.begin(),pl.end(),1) == 1 && count(pl.begin(),pl.end(),2) == 1 && count(pc.begin(),pc.end(),3) == 0){k[n]=3;}else
+                    if(count(pl.begin(),pl.end(),5) == 1 && count(pl.begin(),pl.end(),7) == 1 && count(pc.begin(),pc.end(),3) == 0){k[n]=3;}else
+                    if(count(pl.begin(),pl.end(),1) == 1 && count(pl.begin(),pl.end(),5) == 1 && count(pc.begin(),pc.end(),9) == 0){k[n]=9;}else
+                    if(count(pl.begin(),pl.end(),4) == 1 && count(pl.begin(),pl.end(),7) == 1 && count(pc.begin(),pc.end(),1) == 0){k[n]=1;}else
+                    if(count(pl.begin(),pl.end(),5) == 1 && count(pl.begin(),pl.end(),9) == 1 && count(pc.begin(),pc.end(),1) == 0){k[n]=1;}else
+                    if(count(pl.begin(),pl.end(),5) == 1 && count(pl.begin(),pl.end(),3) == 1 && count(pc.begin(),pc.end(),7) == 0){k[n]=7;}else
+                    if(count(pl.begin(),pl.end(),8) == 1 && count(pl.begin(),pl.end(),9) == 1 && count(pc.begin(),pc.end(),7) == 0){k[n]=7;}else
+                    if(count(pl.begin(),pl.end(),8) == 1 && count(pl.begin(),pl.end(),2) == 1 && count(pc.begin(),pc.end(),5) == 0){k[n]=5;}else
+                    if(count(pl.begin(),pl.end(),6) == 1 && count(pl.begin(),pl.end(),5) == 1 && count(pc.begin(),pc.end(),4) == 0){k[n]=4;}else
+                    if(count(pl.begin(),pl.end(),8) == 1 && count(pl.begin(),pl.end(),5) == 1 && count(pc.begin(),pc.end(),2) == 0){k[n]=2;}else
+                    
+                    {
+                        int y = rand() % diagsize;
+                        p = diag[y];
+                        diagsize--;
+                        cout<<"  Y"<<y<<"  P"<<y;
+                        pc.push_back(p);
+                        for(i = 0;i<pc.size();i++)
+                        {
+                            cout<<pc[i];
+                        }
+                        vec.push_back(p);k[n]=p;
+                        new_end = remove(vic.begin(), vic.end(), p);
+                        new_end = remove(diag.begin(), diag.end(), p);
+                    }
+                    }
+                    //system("clear");
+                
+            }cout<<"  P"<<k[n];
             if(k[n]>6)
             {
                 k[n]-=7;
@@ -80,7 +201,7 @@ cout<<"[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]";
         x+=1;
         y[n]=k[n]/3;
         z[n]=k[n]%3;
-        system("clear");
+        //system("clear");
         for(i=0;i<3;i++)
         {  
             cout<<endl;
@@ -135,7 +256,7 @@ cout<<"[ ][ ][ ]\n[ ][ ][ ]\n[ ][ ][ ]";
         (a[1][0]==2&&a[1][1]==2&&a[1][2]==2)||(a[0][1]==2&&a[1][1]==2&&a[2][1]==2))
         {
             cout<<endl<<name2<<" WINS ";break;
-        }
+        }cout<<n;
         n+=1;b+=1;
     }while(n<10)  ; 
     return 0;
